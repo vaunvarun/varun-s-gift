e5f8775efb25437afb60859c6bd022d50f6dddab
varun M <varuntn45@gmail.com>
2025-01-07 17:09:57 +0530

    add

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add event listener to proposal button
+document.addEventListener("DOMContentLoaded", function() {
+    let proposalButton = document.querySelector(".proposal-button");
+   
+    proposalButton.addEventListener("click", function() {
+     alert("She said yes!");
+    });
+   });
+   
